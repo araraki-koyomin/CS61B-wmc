f58af4324cd16ebaf1524839c9fb3b4427b83588
araraki koyomin <3099146950@qq.com>
2024-03-27 17:36:48 +0800

    New version

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,90 +1,164 @@
 public class ArrayDeque<T> {
-
+    /**Array for deque*/
     private T[] items;
+    /**Deque's size*/
     private int size;
+    /**Deque's length*/
     private int length;
+    /**Front pointer*/
+    private int front;
+    /**Rear pointer*/
+    private int rear;
 
+    /**
+     * Initialize the ArrayDeque
+     */
     public ArrayDeque() {
         items = (T []) new Object[8];
         size = 0;
         length = 8;
+        front = 3;
+        rear = 4;
     }
 
-    public ArrayDeque(ArrayDeque other) {
-        items = (T []) new Object[other.length];
-        size = other.size;
-        length = other.length;
-        for (int i = 0; i < size; i++) {
-            items[i] = (T) other.items[i];
+    /**
+     * Change the ptr forward
+     */
+    public int forwardChange(int index) {
+        if (index == 0) {
+            return length - 1;
+        } else {
+            return index - 1;
         }
     }
 
-    private void check() {
-        while (size < length  * 0.25 && length > 8) {
-            int newLen = (int) (length / 2);
-            T[] tempItems = (T []) new Object[newLen];
-            System.arraycopy(items, 0, tempItems, 0, size);
-            items = tempItems;
+    /**
+     * Change the ptr back
+     */
+    public int backChange(int index) {
+        if (index == length - 1) {
+            return 0;
+        } else {
+            return index + 1;
         }
     }
 
-    private void resise() {
-        T[] tempItems = (T []) new Object[length * 2];
-        System.arraycopy(items, 0, tempItems, 0, size);
-        items = tempItems;
+    /**
+     * Expand the length of deque
+     */
+    public void expand() {
+        T[] newItems = (T[]) new Object[length * 2];
+        int ptr = backChange(front);
+        while (ptr != rear) {
+            newItems[ptr] = items[ptr];
+            ptr = backChange(ptr);
+        }
+        length *= 2;
+        items = newItems;
     }
+
+    /**
+     * Add the item to the front position
+     */
     public void addFirst(T item) {
         if (size == length) {
-            resise();
+            expand();
         }
-        for (int i = size; i >= 1; i--) {
-            items[i] = items[i - 1];
-        }
-        items[0] = item;
+        items[front] = item;
+        front = forwardChange(front);
         size++;
     }
 
+    /**
+     *Add the item to the rear position
+     */
     public void addLast(T item) {
         if (size == length) {
-            resise();
+            expand();
         }
-        items[size] = item;
+        items[rear] = item;
+        rear = backChange(rear);
         size++;
     }
 
+    /**
+     * Check whether the deque is empty
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Get the size of deque
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * print the deque from thr front position to the rear position
+     */
     public void printDeque() {
-        for (int i = 0; i < size; i++) {
-            System.out.print(items[i].toString() + " ");
+        int ptr = backChange(front);
+        while (ptr != rear) {
+            System.out.print(items[ptr].toString() + " ");
+            ptr = backChange(ptr);
         }
         System.out.println();
     }
 
-    public T removeFirst() {
-        for (int i = 0; i < size - 1; i++) {
-            items[i] = items[i + 1];
+    /**
+     * Reduce the length of deque
+     */
+    public void reduce() {
+        T[] newItems = (T[]) new Object[length / 2];
+        int ptr = backChange(front);
+        int newPtr = 0;
+        while (ptr != rear) {
+            newItems[newPtr] = items[ptr];
+            ptr = backChange(front);
         }
-        T x = items[size - 1];
-        items[size - 1] = null;
-        check();
-        return x;
+        items = newItems;
+        length /= 2;
+        front = length - 1;
+        rear = size;
     }
 
+    /**
+     * Remove the item at the front position and return it
+     */
+    public T removeFirst() {
+        if (length >= 16 && size / length < 0.25) {
+            reduce();
+        }
+        front = backChange(front);
+        size--;
+        return items[front];
+    }
+
+    /**
+     * Remove the item at the rear position and return it
+     */
     public T removeLast() {
-        T x = items[size - 1];
-        items[size - 1] = null;
-        check();
-        return x;
+        if (length >= 16 && size / length < 0.25) {
+            reduce();
+        }
+        rear = forwardChange(rear);
+        size--;
+        return items[rear];
     }
 
+    /**
+     * Get the index-th item in deque
+     */
     public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        int ptr = backChange(front);
+        for (int i = 0; i < index; i++) {
+            ptr = backChange(front);
+        }
         return items[index];
     }
 }
